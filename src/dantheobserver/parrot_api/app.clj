d1659e78862fd7a7a4f61af75d02e73eff3ca05f
dantheobserver <daniel.alan.p@gmail.com>
2019-06-23 22:20:17 -0700

    Updated dev-server deps
    * Added dev-server alias for reloading
    * Updated handler to ignore favicon
    * Use mocked db as atom
    
    TODO: convert db to ns with more general access funtions

--- src/dantheobserver/parrot_api/app.clj
+++ src/dantheobserver/parrot_api/app.clj
@@ -1,14 +1,25 @@
 (ns dantheobserver.parrot-api.app
   (:require [dantheobserver.parrot-api.request :as request]
-            [ring.middleware.json :as json]
+            [ring.middleware.json :refer [wrap-json-response]]
+            [ring.middleware.reload :refer [wrap-reload]]
             [ring.util.response :refer [response]]
             [ring.adapter.jetty :as jetty]))
 
-(defn handler [req]
-  (println req)
-  (response {:testing 123}))
+(def db (atom {:api1 {"testing" {:hello "world"}}}))
+(def port 3030)
+
+(defn handler [{:keys [uri] :as req}]
+  (if (= "/favicon.ico" uri)
+    {:status 404}
+    (let [req-data (request/request-data db req)]
+      (println req-data)
+      (response req-data))))
 
 (def app (-> handler
-             json/wrap-json-response))
+             wrap-json-response))
 
-(jetty/run-jetty app {:port 3030})
+(defn run-server []
+  (jetty/run-jetty app {:port port}))
+
+(defn run-dev-server []
+  (jetty/run-jetty (wrap-reload app) {:port port}))
--- src/dantheobserver/parrot_api/request.clj
+++ src/dantheobserver/parrot_api/request.clj
@@ -3,12 +3,13 @@
             [clojure.string :as string]))
 
 (defn entry-path [uri]
-  (let [[_ api-key path-key] (re-matches #"^(.*?)/(.*)" uri)]
+  (let [[_ api-key path-key] (re-matches #"^/(.*?)/(.*)" uri)]
     [(keyword api-key) path-key]))
 
 ;; Request api follows - https://github.com/ring-clojure/ring/blob/master/SPEC
 
 (defn request-data
-  [db {:keys [uri query-string]}]
+  [db {:keys [uri query-string] :as req}]
   (let [path (entry-path uri)]
-    (get-in db path)))
+    (println path)
+    (get-in @db path)))
